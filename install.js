dcb37fd2e25e74248f9bc9ec077b62ef32ab439a
Richie Bendall <richiebendall@gmail.com>
2020-11-16 01:31:59 +1300

    Update Windows Application Driver to 1.2.1
    Signed-off-by: Richie Bendall <richiebendall@gmail.com>

--- install.js
+++ install.js
@@ -4,18 +4,18 @@ const sudo = promisify(require("sudo-prompt").exec)
 const download = require("download")
 const pathExists = require("path-exists")
 const tempWrite = require("temp-write")
-const md5File = require("md5-file")
+const hasha = require("hasha")
 
 const PROGRAM_FILES = process.env["ProgramFiles(x86)"] || process.env.ProgramFiles || "C:\\\\Program Files"
 const INSTALL_PATH = path.resolve(PROGRAM_FILES, "Windows Application Driver", "WinAppDriver.exe")
-const DOWNLOAD_URL = "https://github.com/Microsoft/WinAppDriver/releases/download/v1.2-RC/WindowsApplicationDriver.msi"
-const INSTALLER_MD5 = "dbaa9a3f7416c2b73cc5cd0e7452c8d0"
+const DOWNLOAD_URL = "https://github.com/microsoft/WinAppDriver/releases/download/v1.2.1/WindowsApplicationDriver_1.2.1.msi"
+const INSTALLER_SHA256 = "a76a8f4e44b29bad331acf6b6c248fcc65324f502f28826ad2acd5f3c80857fe"
 
 module.exports = (async () => {
 	if (!(await pathExists(INSTALL_PATH))) {
 		console.log("Installing the Windows Application Driver...")
 		const installerPath = await tempWrite(await download(DOWNLOAD_URL))
-		if (await md5File(installerPath) === INSTALLER_MD5) {
+		if (await hasha.fromFile(installerPath, { algorithm: "sha256" }) === INSTALLER_SHA256) {
 			try {
 				await sudo(`msiexec /i ${installerPath} /qn`, {
 					name: "Windows Application Driver installer"
